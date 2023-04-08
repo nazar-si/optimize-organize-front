e2ae834d2afacb328839c699daf390155c5223b1
nazar-si <nazari61@yandex.ru>
2023-04-08 19:05:47 +0300

    Fixed API error crashing build

--- app/api/registration/route.ts
+++ app/api/registration/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { redirect } from 'next/navigation';
-import apiConfig from '/api.config.js';
+// import apiConfig from '/api.config.js';
 
 export async function POST(request: Request) {
     // const res = await fetch(apiConfig.url + 'users/create-user', {
