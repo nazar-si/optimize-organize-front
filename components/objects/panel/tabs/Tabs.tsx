f53caccf9e0371547ea700c82f2b89ff60837066
nazar-si <nazari61@yandex.ru>
2023-04-09 20:45:31 +0300

    Added buttons

--- components/objects/panel/tabs/Tabs.tsx
+++ components/objects/panel/tabs/Tabs.tsx
@@ -3,15 +3,17 @@ import React, { useState } from 'react'
 import style from "./tabs.module.css";
 import classNames from 'classnames';
 import Task from '../task/Task';
+import { Checkbox, ClipboardData, DeviceComputerCamera, Users } from 'tabler-icons-react';
 
 type Props = {
     children?: React.ReactNode,
 }
 
 const tabs = [
-    {title: "Задачи", value: 0},
-    {title: "Группы", value: 1},
-    {title: "Отчёт",  value: 2},
+    {title: "Задачи", value: 0, icon: <Checkbox size={16}/>},
+    {title: "Группы", value: 1, icon: <Users size={16}/>},
+    {title: "Отчёт",  value: 2, icon: <ClipboardData size={16}/>},
+    {title: "Конференции",  value: 2, icon: <DeviceComputerCamera size={16}/>},
 ]
 
 export default function Tabs({children}:Props) {
@@ -26,42 +28,11 @@ export default function Tabs({children}:Props) {
                             setSelectedTab(i);
                         }}  
                         className={classNames(style.tab, {[style.selected]: i==selectedTab})}>
-                            {e.title}
+                            {e.icon}{e.title}
                     </button>
                 )}
-                <div className={style.line} style={{
-                    left: `${100 / tabs.length * selectedTab}%`,
-                    right: `${100 - (100 / tabs.length) * (selectedTab + 1)}%` 
-                }}/>
-            </header>
-            <main className={classNames(style.tabBody, {[style.selected]: selectedTab==0})}>
-                <div className={style.taskList}>
-                    <Task data={{
-                        id: "1",
-                        done: false,
-                        name: "Hello",
-                        peopleCount: 1,
-                        attributesCount: 1,
-                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor temporibus illum aliquid numquam laboriosam in quis quaerat aspernatur possimus, dicta, ea, cum voluptates eos suscipit odit magni maiores placeat dolores!",
-                        date: new Date(),
-                    }}/>
-                    <Task data={{
-                        id: "2",
-                        done: true,
-                        name: "Hello Done",
-                        peopleCount: 3,
-                        attributesCount: 5,
-                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor temporibus illum aliquid numquam laboriosam in quis quaerat aspernatur possimus, dicta, ea, cum voluptates eos suscipit odit magni maiores placeat dolores!",
-                        date: new Date()
-                    }}/>
-                </div>
-            </main>
-            <main className={classNames(style.tabBody, {[style.selected]: selectedTab==1})}>
-                Группы
-            </main>
-            <main className={classNames(style.tabBody, {[style.selected]: selectedTab==2})}>
-                Отчёт
-            </main>
+
+            </header> 
         </div>
     )
 }
