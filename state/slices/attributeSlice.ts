fb96829cbab13e12e9b7d17d8d96e7a318cbbbd9
nazar-si <nazari61@yandex.ru>
2023-04-09 05:28:35 +0300

    Finished Redux store

--- state/slices/attributeSlice.ts
+++ state/slices/attributeSlice.ts
@@ -1,6 +1,7 @@
 import { GeneralAttribute, ID } from "@/components/attributes/types";
 import { createSlice } from "@reduxjs/toolkit";
 import type {Action} from "@reduxjs/toolkit"
+import { prototype } from "events";
 
 export type stateType = {
     attributes: Map<ID, GeneralAttribute>; 
@@ -10,15 +11,66 @@ const initialState:stateType = {
     attributes: new Map<ID, GeneralAttribute>()
 };
 
-export const attributesSlice = createSlice({
+type reducersType = {
+    // Подгрузить начальные атрибуты, чтобы отобразить 
+    load: (state: stateType, action: {type: string, payload: Array<GeneralAttribute>}) => void
+    // Удалить значение атрибута (attribute_value)
+    remove: (state: stateType, action: {type: string, payload: ID}) => void 
+    // Создать значение атрибута
+    add: (state: stateType, action: {type: string, payload: {
+        prototype: GeneralAttribute,
+        // ... Параметры, которые нужны бэкенду
+    }}) => void,
+    // Обновить значение атрибута 
+    update: (state: stateType, action: {type: string, payload: {
+        id: ID,
+        params: Partial<GeneralAttribute>, // часть параметров из основного атрибута
+        // ... Параметры, которые нужны бэкенду
+    }}) => void
+}
+
+
+export const attributesSlice = createSlice<stateType, reducersType>({
     name: "attributesSlice",
     initialState,
     reducers: {
-        load: (state: stateType, action : {type: string, payload: Array<GeneralAttribute>}) => {
+        load: (state, action) => {
             action.payload.forEach(a => {
                 state.attributes.set(a.ID, a);
             })
         },
+        remove: (state, action) => {
+            state.attributes.delete(action.payload);
+            
+            // И код, чтобы удалить из БД: 
+            // ... 
+        },
+        add: (state, action) => {
+            // Здесь код на создание значения атрибута на бэке 
+            // ... 
+            
+            // ID нового атрибута: 
+            const id = Math.floor(Math.random() * 1e10) 
+
+            // Прототип атрибута - шаблон с базовыми данными
+            // Можно использовать, чтобы загрузить начальные данные на бэк
+            let newAttribute : GeneralAttribute = action.payload.prototype; // создали объект по прототипу
+            newAttribute.ID = id // присвоили id из "бэка"
+            state.attributes.set(id, newAttribute) 
+        },
+        update: (state, action) => {
+            const id = action.payload.id; 
+            let entry = state.attributes.get(id);
+            if (entry === undefined)
+                return
+
+            // Новый объект 
+            let obj : GeneralAttribute = {...entry, ...action.payload.params }
+            state.attributes.set(id, obj); 
+
+            // И код, чтобы обновить на БД: 
+            // ... 
+        }
     },
 });
 
