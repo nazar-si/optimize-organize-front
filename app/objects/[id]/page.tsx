7930dc909357c7bfdd1f7f0f40aa6742d5172dfd
Al1en <39908226+Al-1-en@users.noreply.github.com>
2023-04-10 00:16:07 +0300

    add fetch for attributes

--- app/objects/[id]/page.tsx
+++ app/objects/[id]/page.tsx
@@ -1,9 +1,11 @@
+'use client'
 import { NextPage } from "next";
 import { useRouter } from "next/navigation";
 import Editor from "@/components/attributes/editor/Editor";
 import Topbar from "@/components/attributes/topbar/Topbar";
 import { GeneralAttribute } from "@/components/attributes/types";
 import { IData } from "./object.type";
+import { useEffect, useState } from "react";
 
 type dataType = {
   basicData: {
@@ -12,26 +14,52 @@ type dataType = {
   attributes: Array<GeneralAttribute> 
 }
 
-async function getData(id:number): Promise<dataType> {
-  // здесь мы тип от бэка получили массив атрибутов 
-  const data: dataType = {
-    basicData: {
-      name: 'МГУ',
+const proto: dataType = {
+  basicData: {
+    name: '',
+  },
+  attributes: []
+}; 
+
+async function getData(id:string, token: string | null): Promise<dataType> {
+  if(!token || !id)
+    return proto;
+
+  const res = await fetch('/api/getAttributesById', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-    attributes: []
-  }; 
-  return data; 
+    body: JSON.stringify({
+      id: id,
+      token: token
+    })
+  });
+  if(!res.ok)
+    return proto;
+  try{
+    const data = await res.json();
+    return data;
+  }
+  catch(e){
+    return proto;
+  }
 }
 
-export default async function Page({
+export default function Page({
     params,
     searchParams,
   }: {
     params: { id: string };
     searchParams: { [key: string]: string | string[] | undefined };
   }) {
-
-    const data = await getData(parseInt(params.id));
+    const [data, setData] = useState<dataType>(proto);
+    useEffect(() => {
+      getData(params.id, window.localStorage.getItem('token')).then(data => setData(data));
+    }, [params.id]);
+    useEffect(() => {
+      console.log(data)
+    }, [data]);
     return <div className="flex flex-col h-[100vh]">
         <Topbar passName={data.basicData.name}></Topbar>
         <Editor upload={data.attributes /* Загружаем значения атрибутов в редактор */}></Editor> 
--- app/objects/building.type.ts
+++ app/objects/building.type.ts
@@ -4,5 +4,23 @@ export type Building = {
     UpdatedAt: string,
     DeletedAt: string,
     groups: any[],
-    attributes: any[],
+    attributes: Array<{
+        ID: number,
+        CreatedAt: string,
+        UpdatedAt: string | null,
+        DeletedAt: string | null,
+        attribute_id: number,
+        attribute: {
+            ID: number,
+            CreatedAt: string,
+            UpdatedAt: string | null,
+            DeletedAt: string | null,
+            attr_name: string,
+            attr_description: string,
+            attr_type: number,
+            modify_permission: number
+        },
+        building_id: number,
+        data: string
+    }>,
 }
